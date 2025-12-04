bad15d5ebf2f09d64761839c12e40d3e55719ef7
skraipy <sa957635@gmail.com>
2025-12-04 20:18:03 +0300

    Была добавлена основа для веб-сервиса

---
+++ frontend/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Creator from "./Creator.tsx";
+function App() {
+  return (
+    <div>
+      <h1>ETU Forms works</h1>
+      <Creator /> {/* Здесь отображается SurveyJS конструктор */}
+    </div>
+  );
+}
+export default App;
---
+++ frontend/frontend/src/Creator.tsx
@@ -0,0 +1,13 @@
+import "./style/survey-custom.css";
+
+import { SurveyCreator, SurveyCreatorComponent } from "survey-creator-react";
+
+const Creator = () => {
+  const creator = new SurveyCreator({ showLogicTab: false });
+
+  // @ts-ignore — временное решение для TypeScript
+  return <SurveyCreatorComponent creator={creator} />; 
+};
+
+export default Creator;
+
