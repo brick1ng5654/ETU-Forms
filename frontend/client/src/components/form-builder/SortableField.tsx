79c0d4469d8a124cb48f686d282cf80154a373bb
Skraipy <sa957635@gmail.com>
2026-01-04 21:00:17 +0300

    Изменен чекбокс

--- frontend/client/src/components/form-builder/SortableField.tsx
+++ frontend/client/src/components/form-builder/SortableField.tsx
@@ -14,6 +14,7 @@ import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { useTranslation } from 'react-i18next';
 import { Languages } from "lucide-react";
 
+
 interface SortableFieldProps {
   field: FormField;
   isSelected: boolean;
