6080384a5f53fc80d4c8535587c1e36eda118cc4
Леонид <Leopas0511@gmail.com>
2026-01-22 14:07:15 +0300

    Добавил поле required_field Обязательное поле

--- database/init/01-init.sql
+++ database/init/01-init.sql
@@ -178,6 +178,7 @@ CREATE TABLE IF NOT EXISTS Form_Element (
     correct_answer JSONB NULL,
     text_hint TEXT NULL,
     supportive_text TEXT NULL,
+    required_field BOOLEAN NOT NULL,
     other_settings JSONB,
 
     CONSTRAINT fk_element_form
