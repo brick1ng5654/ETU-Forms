07404ab1a5a2f3ac384d0ab2a83550160f9385b6
Леонид <Leopas0511@gmail.com>
2026-04-17 07:50:13 +0300

    349-Убрал лишние символы и строки. Убрал символ, который появлялся рядом с строками. Убрал страницу с кодовым представлением. Убрал содержимое футера.

--- docs-sphinx/docs/source/conf.py
+++ docs-sphinx/docs/source/conf.py
@@ -13,6 +13,14 @@ language = 'ru'
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
+html_show_sphinx = False
+html_show_sourcelink = False
+
+# Furo: remove "view this page" / "view source" top-of-page button.
+html_theme_options = {
+    "top_of_page_buttons": [],
+}
 
 source_suffix = {
     '.md': 'markdown',
